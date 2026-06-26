1d0fca01e44dd98892e802da0601b546e3b7b0f1
Gonzalo <gfernandez@cybelesoft.com>
2026-06-26 11:34:46 -0300

    [config] Update todo.txt filename convention

--- lua/plugins/todo.lua
+++ lua/plugins/todo.lua
@@ -4,8 +4,14 @@ return {
     init = function()
       vim.filetype.add({
         pattern = {
-          [".*todo%.txt"] = "todo",
+          -- name starts with "todo" and ends in .txt (todo.txt, todo.work.txt, ...)
+          ["todo[^/\\]*%.txt"] = "todo",
+          [".*[/\\]todo[^/\\]*%.txt"] = "todo",
+          -- *to-do.txt
           [".*to%-do%.txt"] = "todo",
+          -- meetings.txt anywhere
+          ["meetings%.txt"] = "todo",
+          [".*[/\\]meetings%.txt"] = "todo",
         },
       })
     end,
