8df99924e429f331bc69f57865407e48c9326ff9
Gonzalo <gfernandez@cybelesoft.com>
2026-09-18 12:21:48 -0300

    add bufferline move commands

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+-- Move the current buffer in the bufferline
+map("n", "<leader>bH", "<cmd>BufferLineMovePrev<cr>", { desc = "Move Buffer Left" })
+map("n", "<leader>bL", "<cmd>BufferLineMoveNext<cr>", { desc = "Move Buffer Right" })
